cc92488d21b57266c17eb67c1fbd0a818f58d6cc
viniciusmilan8 <viniciusmilan8@gmail.com>
2021-09-29 21:49:14 -0300

    confirmar

---
+++ js/start-reactor.js
@@ -0,0 +1,12 @@
+startReactor = {
+
+    computerCombination: [],
+    playerCombination: [],
+    computerCombinationPosition: 1,
+    computerMaxPosition: 5,
+    memoryMaxCombination: 9,
+
+    audio : {},
+    interface: {},
+
+}
